e652f61e2902c9438443508a8c1d19315202fd4c
Lucas Corte <corte1994.lc@gmail.com>
2021-07-19 10:42:04 -0300

    inserção do evendo para compor

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 const body = document.body;
+const composerButton = document.querySelector('.composer button');
 
 body.addEventListener('keyup', (e) => {
   playSound(e.code.toLowerCase());
+});
+
+composerButton.addEventListener('click', (e) => {
+  let song = document.querySelector('#input').value;
+
+  if (song !== '') {
+    let songArray = song.split('')
+    playComposition(songArray);
+  }
 });
 
 const playSound = (sound) => {
